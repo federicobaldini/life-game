c98a26d5bef7fac9a4b2929c706efcd32e731b75
Federico Baldini <federico.baldini7@gmail.com>
2022-09-17 13:19:49 +0200

    Update lib.rs
    Added the live_neighbor_count() function.

--- src/lib.rs
+++ src/lib.rs
@@ -8,16 +8,16 @@ use wasm_bindgen::prelude::*;
 #[repr(u8)]
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Cell {
-    Dead = 0,
-    Alive = 1,
+  Dead = 0,
+  Alive = 1,
 }
 
 // The universe has a width and a height, and a vector of cells of length width * height.
 #[wasm_bindgen]
 pub struct Universe {
-    width: u32,
-    height: u32,
-    cells: Vec<Cell>,
+  width: u32,
+  height: u32,
+  cells: Vec<Cell>,
 }
 
 // The universe can be rappresented as a flat array that lives in the WebAssembly linear memory,
@@ -32,8 +32,31 @@ impl Universe {
   // To access the cell at a given row and column, we translate the row and column into an
   // index into the cells vector
   fn get_index(&self, row: u32, column: u32) -> usize {
-      (row * self.width + column) as usize
+    (row * self.width + column) as usize
   }
 
-  // ...
+  // In order to calculate the next state of a cell, it's needed to get a count of how many
+  // of its neighbors are alive.
+  //
+  // The live_neighbor_count method uses deltas and modulo to avoid special casing the edges of the
+  // universe with ifs. When applying a delta of -1, we add self.height - 1 and let the modulo do its
+  // thing, rather than attempting to subtract 1. row and column can be 0, and if we attempted to
+  // subtract 1 from them, there would be an unsigned integer underflow.
+  fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+    let mut count = 0;
+    // [self.height - 1, 0, 1].iter().cloned()
+    for delta_row in [self.height - 1, 0, 1] {
+      for delta_column in [self.width - 1, 0, 1] {
+        if delta_row == 0 && delta_column == 0 {
+          continue;
+        }
+        let neighbor_row = (row + delta_row) % self.height;
+        let neighbor_column = (column + delta_column) % self.width;
+        let index = self.get_index(neighbor_row, neighbor_column);
+        count += self.cells[index] as u8;
+      }
+    }
+    count
+  }
 }
+
