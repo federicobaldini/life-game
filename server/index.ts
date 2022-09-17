e4f1c360c0dc6ea45017d81e1b1edb53eddb0593
Federico Baldini <federico.baldini7@gmail.com>
2022-09-17 13:58:10 +0200

    Update index.ts
    Created the drawCells() function.

--- server/index.ts
+++ server/index.ts
@@ -28,6 +28,46 @@ const drawGrid = (
   canvasContext.stroke();
 };
 
+const getIndex = (row: number, column: number, width: number) => {
+  return row * width + column;
+};
+
+const drawCells = (
+  universe: Universe,
+  canvasContext: CanvasRenderingContext2D,
+  wasmMemory: WebAssembly.Memory,
+  width: number,
+  height: number
+) => {
+  const cellsPtr = universe.cells();
+  const cells = new Uint8Array(wasmMemory.buffer, cellsPtr, width * height);
+
+  canvasContext.beginPath();
+
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col, width);
+
+      canvasContext.fillStyle =
+        cells[idx] === Cell.Dead ? DEAD_COLOR : ALIVE_COLOR;
+
+      canvasContext.fillRect(
+        /*
+        With grid:
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        */
+        col * CELL_SIZE,
+        row * CELL_SIZE,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
+
+  canvasContext.stroke();
+};
+
 init().then((wasm: InitOutput) => {
   const pre = document.getElementById("game-of-life-canvas");
   const universe = Universe.new();
