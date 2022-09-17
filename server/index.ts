7680d6dac4c9812779120556a5cf4074b73a780b
Federico Baldini <federico.baldini7@gmail.com>
2022-09-17 13:34:36 +0200

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,18 @@
-import init, { InitOutput } from "life-game";
+import init, { InitOutput, Universe } from "life-game";
 
-init().then((wasm: InitOutput) => {});
+init().then((wasm: InitOutput) => {
+  const pre = document.getElementById("game-of-life-canvas");
+  const universe = Universe.new();
+
+  const renderLoop = () => {
+    const frame_per_second: number = 10;
+    setTimeout(() => {
+      pre.textContent = universe.render();
+      universe.update();
+
+      requestAnimationFrame(renderLoop);
+    }, 1000 / frame_per_second);
+  };
+
+  requestAnimationFrame(renderLoop);
+});
