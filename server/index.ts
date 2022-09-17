cfedb64d0a12b6f44f80dbd3b0936c8f74a7b886
Federico Baldini <federico.baldini7@gmail.com>
2022-09-17 13:57:45 +0200

    Update index.ts
    Created the drawGrid() function.

--- server/index.ts
+++ server/index.ts
@@ -5,6 +5,29 @@ const GRID_COLOR = "#494949";
 const DEAD_COLOR = "#494949";
 const ALIVE_COLOR = "#fdf8d8";
 
+const drawGrid = (
+  canvasContext: CanvasRenderingContext2D,
+  width: number,
+  height: number
+) => {
+  canvasContext.beginPath();
+  canvasContext.strokeStyle = GRID_COLOR;
+
+  // Vertical lines.
+  for (let i = 0; i <= width; i++) {
+    canvasContext.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+    canvasContext.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+  }
+
+  // Horizontal lines.
+  for (let j = 0; j <= height; j++) {
+    canvasContext.moveTo(0, j * (CELL_SIZE + 1) + 1);
+    canvasContext.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+  }
+
+  canvasContext.stroke();
+};
+
 init().then((wasm: InitOutput) => {
   const pre = document.getElementById("game-of-life-canvas");
   const universe = Universe.new();
