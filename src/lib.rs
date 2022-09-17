c63d8760b8d1ad3fceff1b3740bb285e6abb6738
Federico Baldini <federico.baldini7@gmail.com>
2022-09-17 13:28:23 +0200

    Update lib.rs
    Added the update function that calculate the next generation of cells for the Universe.

--- src/lib.rs
+++ src/lib.rs
@@ -106,5 +106,36 @@ impl Universe {
     }
     count
   }
-}
 
+  pub fn update(&mut self) {
+    let mut next = self.cells.clone();
+
+    for row in 0..self.height {
+      for col in 0..self.width {
+        let cell_index = self.get_index(row, col);
+        let cell = self.cells[cell_index];
+        let live_neighbors = self.live_neighbor_count(row, col);
+
+        let next_cell = match (cell, live_neighbors) {
+          // Rule 1: Any live cell with fewer than two live neighbours
+          // dies, as if caused by underpopulation.
+          (Cell::Alive, x) if x < 2 => Cell::Dead,
+          // Rule 2: Any live cell with two or three live neighbours
+          // lives on to the next generation.
+          (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+          // Rule 3: Any live cell with more than three live
+          // neighbours dies, as if by overpopulation.
+          (Cell::Alive, x) if x > 3 => Cell::Dead,
+          // Rule 4: Any dead cell with exactly three live neighbours
+          // becomes a live cell, as if by reproduction.
+          (Cell::Dead, 3) => Cell::Alive,
+          // All other cells remain in the same state.
+          (otherwise, _) => otherwise,
+        };
+
+        next[cell_index] = next_cell;
+      }
+    }
+    self.cells = next;
+  }
+}
