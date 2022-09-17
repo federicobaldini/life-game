f845bd60e6ac19b95412e22cb43af91901032f7e
Federico Baldini <federico.baldini7@gmail.com>
2022-09-17 13:57:24 +0200

    Update index.ts
    Defined universe constants.

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,9 @@
-import init, { InitOutput, Universe } from "life-game";
+import init, { InitOutput, Universe, Cell } from "life-game";
+
+const CELL_SIZE = 5; // px
+const GRID_COLOR = "#494949";
+const DEAD_COLOR = "#494949";
+const ALIVE_COLOR = "#fdf8d8";
 
 init().then((wasm: InitOutput) => {
   const pre = document.getElementById("game-of-life-canvas");
